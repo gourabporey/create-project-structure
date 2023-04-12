c48e0175e5941d044244462c5530671a3b9a0b9a
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-12 17:22:24 +0530

    Added relative paths of snippets

--- src/create-project.sh
+++ src/create-project.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-source ~/my-utils/create-project-structure/lib/case-conversion.sh
+source ./create-project-structure/lib/case-conversion.sh
 
 function create_project() {
   local PROJECT_NAME=$1;
@@ -11,12 +11,12 @@ function create_project() {
   local FUNCTION_NAME=$(kebab_to_camel_case $PROJECT_NAME);
   local FUNCTION_NAME_IN_PC=$(kebab_to_pascal_case $PROJECT_NAME);
 
-  cat ~/my-utils/create-project-structure/lib/testingFramework.js > "lib/testing.js"
-  cat ~/my-utils/create-project-structure/lib/functionSnippet.js > "src/$PROJECT_NAME.js"
-  cat ~/my-utils/create-project-structure/lib/testSnippet.js | sed -e "s/FILE_IN_CC/$FUNCTION_NAME/g" -e "s/FILE_IN_PC/$FUNCTION_NAME_IN_PC/g" -e "s/PROJECT/$PROJECT_NAME/g" > "test/$PROJECT_NAME-test.js"
+  cat ../create-project-structure/lib/testingFramework.js > "lib/testing.js"
+  cat ../create-project-structure/lib/functionSnippet.js > "src/$PROJECT_NAME.js"
+  cat ../create-project-structure/lib/testSnippet.js | sed -e "s/FILE_IN_CC/$FUNCTION_NAME/g" -e "s/FILE_IN_PC/$FUNCTION_NAME_IN_PC/g" -e "s/PROJECT/$PROJECT_NAME/g" > "test/$PROJECT_NAME-test.js"
 
-  cp ~/my-utils/create-project-structure/lib/runtest.sh .;
-  open -a "iTerm"; clear;
+  cp ../create-project-structure/lib/runtest.sh .;
+  clear;
   ./runtest.sh;
 }
 
