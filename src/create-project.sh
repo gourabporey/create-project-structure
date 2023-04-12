588f0438a985edba5edceb9269dc91a6e8b7d4ed
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-12 11:51:55 +0530

    Added test framework snippet in library

--- src/create-project.sh
+++ src/create-project.sh
@@ -1,22 +1,21 @@
 #! /bin/bash
-source lib/case-conversion.sh
+source ~/my-utils/create-project-structure/lib/case-conversion.sh
 
 function create_project() {
   local PROJECT_NAME=$1;
 
-  mkdir $PROJECT_NAME;
-  cd $PROJECT_NAME;
+  mkdir $PROJECT_NAME; cd $PROJECT_NAME;
 
   mkdir lib src test;
 
   local FUNCTION_NAME=$(kebab_to_camel_case $PROJECT_NAME);
   local FUNCTION_NAME_IN_PC=$(kebab_to_pascal_case $PROJECT_NAME);
 
-  cat ~/bin/testingFramework.js > "lib/testing.js"
-  cat ~/bin/jslib/functionSnippet.js > "src/$PROJECT_NAME.js"
-  cat ~/bin/jslib/testSnippet.js | sed -e "s/FILE_IN_CC/$FUNCTION_NAME/g" -e "s/FILE_IN_PC/$FUNCTION_NAME_IN_PC/g" -e "s/PROJECT/$PROJECT_NAME/g" > "test/$PROJECT_NAME-test.js"
+  cat ~/my-utils/create-project-structure/lib/testingFramework.js > "lib/testing.js"
+  cat ~/my-utils/create-project-structure/lib/functionSnippet.js > "src/$PROJECT_NAME.js"
+  cat ~/my-utils/create-project-structure/lib/testSnippet.js | sed -e "s/FILE_IN_CC/$FUNCTION_NAME/g" -e "s/FILE_IN_PC/$FUNCTION_NAME_IN_PC/g" -e "s/PROJECT/$PROJECT_NAME/g" > "test/$PROJECT_NAME-test.js"
 
-  cp ~/bin/shlib/runtest.sh .;
+  cp ~/my-utils/create-project-structure/lib/runtest.sh .;
   open -a "iTerm"; clear;
   ./runtest.sh;
 
